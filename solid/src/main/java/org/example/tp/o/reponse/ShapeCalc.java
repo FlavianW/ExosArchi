d51dbfc9426afcc8934fa58dbf4679c56f3c6054
FlavianW <flavian.waroquier@uphf.fr>
2024-11-19 11:43:30 +0100

    Ajout reponse o

---
+++ solid/src/main/java/org/example/tp/o/reponse/ShapeCalc.java
@@ -0,0 +1,57 @@
+package org.example.tp.o.reponse;
+
+import java.util.List;
+
+public class ShapeCalc {
+    public double calculateTotalArea(List<Shape> shapes) {
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.calculateArea();
+        }
+        return totalArea;
+    }
+}
+
+abstract class Shape {
+    public abstract double calculateArea();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
