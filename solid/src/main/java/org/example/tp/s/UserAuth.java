b34e744618370eb52a1fae7060f15fbdf7a3ac1e
FlavianW <flavian.waroquier@uphf.fr>
2024-11-19 09:46:48 +0100

    Ajout de l'exo SOLID

---
+++ solid/src/main/java/org/example/tp/s/UserAuth.java
@@ -0,0 +1,8 @@
+package org.example.tp.s;
+
+public class UserAuth {
+    public void authenticate(String username, String password) {
+        // Authentifier l'utilisateur (simulé ici par un message)
+        System.out.println("Utilisateur " + username + " connecté avec succès.");
+    }
+}
---
+++ solid/src/main/java/org/example/tp/s/UserRegistration.java
@@ -0,0 +1,8 @@
+package org.example.tp.s;
+
+public class UserRegistration {
+    public void register(String username, String password) {
+        // Enregistrer l'utilisateur dans la base de données (simulé ici par un message)
+        System.out.println("Utilisateur " + username + " enregistré avec succès.");
+    }
+}
