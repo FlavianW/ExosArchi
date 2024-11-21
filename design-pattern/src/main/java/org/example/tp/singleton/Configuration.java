0dc3c02fa3903ca0f6dda0c39242a03e8bfb9d20
FlavianW <flavian.waroquier@uphf.fr>
2024-11-21 10:24:15 +0100

    Ajout Singleton

---
+++ design-pattern/src/main/java/org/example/tp/singleton/Configuration.java
@@ -0,0 +1,39 @@
+package org.example.tp.singleton;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class Configuration {
+
+    Map<String, String> settings;
+
+    private static Configuration instance;
+
+    private Configuration() {
+        this.settings = new HashMap<>();
+        this.settings.put("bdd","abcd");
+        this.settings.put("env","abcd");
+        this.settings.put("api","1234");
+    }
+
+    private String getSetting(String key){
+        return this.settings.get(key);
+    }
+
+    private void setSettings(String key, String value){
+        this.settings.put(key,value);
+    }
+
+    public static Configuration getInstance(){
+        if (instance == null) {
+            instance = new Configuration();
+        }
+        return instance;
+    }
+}
+
+
--- design-pattern/src/main/java/org/example/tp/singleton/Demo.java
+++ design-pattern/src/main/java/org/example/tp/singleton/Demo.java
@@ -2,8 +2,8 @@ package org.example.tp.singleton;
 
 public class Demo {
     public static void main(String[] args) {
-        ConfigManagementService config1 = ConfigManagementService.getInstance("bdd1", "abcd", "1234");
-        ConfigManagementService config2 = ConfigManagementService.getInstance("aaaa","aaaa","aaaa");
+        Configuration config1 = Configuration.getInstance();
+        Configuration config2 = Configuration.getInstance();
         System.out.println(config1 == config2);
     }
 }
